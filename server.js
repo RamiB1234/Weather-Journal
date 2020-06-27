f1b1114b229ac4ac930f41f5dcc04b6a865dbe62
Rami Bokhari <rbisthebest@hotmail.com>
2020-06-27 18:09:27 +0300

    Implement the API calls

--- server.js
+++ server.js
@@ -2,15 +2,47 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
+
+const port = 3000;
+
+// Create a local server
+const server = app.listen(port, listening);
+
+// Listening function
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);
+}
+
+// Get method route
+// Return data from projectData
+app.get('/data', function (req, res) {
+    res.send(projectData);
+  })
+
+// POST method route
+// Assign data to projectData
+app.post('/addUserData', function (req, res) {
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.comment = req.body.comment;
+
+    res.send(projectData);
+  })
--- website/app.js
+++ website/app.js
@@ -1,5 +1,67 @@
 /* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = 'f9a3936b8f0a2f9f2672175fa16d7d91';
+let zipCode = '94040'; // should be coming from UI
+
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Fetch data from openweathermap API
+const getTemp = async (zip)=>{
+
+    const res = await fetch(baseURL+zip+',us&appid='+apiKey)
+    try {
+  
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+
+  const sendUserData= async (url, data={})=>{
+
+    const res = await fetch(url, {
+        method : "POST",
+        credentials : "same-origin",
+        headers : {
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+  
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }  catch(error) {
+      console.log(res);
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+
+  // Get data from local server
+const getData = async (url)=>{
+
+  const res = await fetch(url)
+  try {
+
+    const data = await res.json();
+    console.log(data)
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+  getTemp(zipCode);
+  //getData('http://localhost:3000/data');
+  sendUserData('http://localhost:3000/addUserData', {temperature: "26", date: newDate, comment: 'test'})
